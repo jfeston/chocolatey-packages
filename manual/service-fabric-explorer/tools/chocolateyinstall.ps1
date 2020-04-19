d12ae799c95dc66bb4c31bef4551325eb41b0124
Javier <jfeston@users.noreply.github.com>
2020-04-19 15:56:51 -0700

    Add service-fabric-explorer package

---
+++ manual/service-fabric-explorer/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/microsoft/service-fabric-explorer/releases/download/v18.9.2701/Windows-18.9.2701.x86.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI' #only one of these: exe, msi, msu
+  url           = $url
+  softwareName  = 'Service Fabric Explorer'
+  checksum      = 'F1DBDBD345D69D1139898AFF1E8FE04DCAF0621B4E9E59F87B230D342B18DE5C'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
