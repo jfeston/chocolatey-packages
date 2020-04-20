c3b2af35c9f9d7bf765b5e35b53f4b07decfbe7d
Javier Feston Cardenas <jfeston@users.noreply.github.com>
2020-04-20 06:28:03 -0700

    Add NXZT CAM package (#2)
    * Add NXZT CAM package
    * Update nzxt package with required info

---
+++ manual/nzxt/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://nzxt-app.nzxt.com/NZXT%20CAM%20Setup%204.5.3.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'nzxt*'
+  checksum      = '5A91AB2ADAA48287E3A3931B09971377667D745694594A51876FC1BB5F7A42FD'
+  checksumType  = 'sha256'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
