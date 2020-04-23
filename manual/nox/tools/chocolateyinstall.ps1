55978f852a4351e1db28d9845fe848fe36d3b41b
Javier <jfeston@users.noreply.github.com>
2020-04-23 14:59:10 -0700

    Add nox player package Add luminar package

---
+++ manual/nox/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://res06.bignox.com/full/20200418/20ce570a66c649bfb1a26240b98d3c55.exe?filename=nox_setup_v6.6.0.5_full_intl.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'nox*'
+  checksum      = '88508D9E5FCB58E58FD2A2FE5EA6BC1A6594D9DD926899877E0D4730E3F56FD6'
+  checksumType  = 'sha256'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ manual/nox/tools/chocolateyuninstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'nox*'
+  fileType      = 'EXE'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $_.UninstallString -match '"(?<file>.*).exe" (?<args>.*)'
+    $packageArgs['file'] = "$($matches['file']).exe"
+    $packageArgs['silentArgs'] = "$($packageArgs['silentArgs']) /$($matches['args'])"
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
