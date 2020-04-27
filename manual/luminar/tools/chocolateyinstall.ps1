9fc5156d16c2282f283130ba8721dcd23543d257
Javier Feston Cardenas <jfeston@users.noreply.github.com>
2020-04-27 08:59:14 -0700

    Add nox player package (#4)
    Add luminar package

---
+++ manual/luminar/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://downloads.skylum.com/luminar/win/win_installer/Luminar3_Setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'luminar*'
+  checksum      = '8946DE177881111EC5613B4A5BAC314EF45B83A690557C6287E4892D474AF871'
+  checksumType  = 'sha256'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
