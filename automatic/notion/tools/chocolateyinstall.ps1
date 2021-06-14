71c0a2f0206943207894c5aa52b4f074ce5c8d77
Ryan Caezar Itang <sitiom@disroot.org>
2021-06-14 21:06:35 +0800

    notion: Update to 2.0.16

--- automatic/notion/tools/chocolateyinstall.ps1
+++ automatic/notion/tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://desktop-release.notion-static.com/Notion%20Setup%202.0.11.exe'
+$url64      = 'https://desktop-release.notion-static.com/Notion%20Setup%202.0.16.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url64
   softwareName   = 'notion*'
-  checksum64     = '4528BB49AB677EB73B84DA3A60840F6D1081FF0AFD5C2D64EF6247058A38C19D'
+  checksum64     = '26825c3b8de2138dd3d04528de37c2b105c597a86c0d597a40fb31897789b96a'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
